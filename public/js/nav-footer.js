8fb104fb64967c13273047d38b0ce96a6c7515f0
ChristopherSaury <saurychristopher@gmail.com>
2022-09-30 14:13:22 +0200

    user navbar

--- public/js/nav-footer.js
+++ public/js/nav-footer.js
@@ -13,6 +13,23 @@ const toggleMobileMenu = (element) => {
         element.nextElementSibling.classList.add("open");
     }
 }
+// USER NAVBAR
+let userNav = document.querySelector(".user-menu .user-menu-icon");
+let arrowUser = document.querySelector(".user-menu .user-menu-icon #lg-arrow-user");
+let userNavMenu = document.querySelector(".user-menu .user-config");
+
+    const toggleUserMenu = (element) => {
+        if(element.nextElementSibling.classList.contains("close-user-menu")){
+            userNavMenu.classList.remove("close-user-menu");
+            userNavMenu.style.display = 'flex';
+            arrowUser.style.transform = "rotate(180deg)";
+        }else if (!element.nextElementSibling.classList.contains("close-user-menu")){
+            userNavMenu.classList.add("close-user-menu");
+            userNavMenu.style.display = 'none';
+            arrowUser.style.transform = "rotate(0deg)";
+        }
+    }
+
 
 // FOOTER
 const currentYear = new Date().getFullYear();
