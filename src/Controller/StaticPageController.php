316e589c0ffc4401a8e2815c82e03daa591e62ef
ChristopherSaury <saurychristopher@gmail.com>
2022-09-30 14:14:43 +0200

    newsletter form

--- src/Controller/StaticPageController.php
+++ src/Controller/StaticPageController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Newsletter;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StaticPageController extends AbstractController{
     #[Route("/", name:"accueil")]
@@ -11,5 +13,19 @@ class StaticPageController extends AbstractController{
         return $this->render('static/home.html.twig',[
             'controller_name' => 'StaticPageController'
         ]);
+    }
+    #[Route("/newsletter/handler", name:"add_newsletter")]
+    public function newsletter(EntityManagerInterface $em){
+        if(!empty($_POST['nsl-subscription']) && !empty($_POST['nsl-agreement-box'])){
+            $new_sub = new Newsletter;
+            $new_sub->setSubscriptionEmail($_POST['nsl-subscription']);
+            $new_sub->setTermOfUse($_POST['nsl-agreement-box']);
+            $em->persist($new_sub);
+            $em->flush();
+
+            return $this->redirectToRoute('accueil');
+        }else{
+            return $this->redirectToRoute('accueil');
+        }
     }
 }
