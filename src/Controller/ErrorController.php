79b6b98515808498daa5231260014ef7bfc1b967
ChristopherSaury <saurychristopher@gmail.com>
2022-10-20 13:46:27 +0200

    symfony twig pack added + Error page updated

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -8,14 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ErrorController extends AbstractController{
     #[Route('/erreur/404', name:'error_404')]
     public function Error404(){
-        return $this->render('error/error404.html.twig');
+        return $this->render('error/error404page.html.twig');
     }
     #[Route('/erreur/403', name:'error_403')]
     public function Error403(){
-        return $this->render('error/error403.html.twig');
+        return $this->render('error/error403page.html.twig');
     }
     #[Route('/erreur/500', name:'error_500')]
     public function Error500(){
-        return $this->render('error/error500.html.twig');
+        return $this->render('error/error500page.html.twig');
     }
 }
